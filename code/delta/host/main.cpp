f8fc31a8e4d7116679be2b03f7b9dbd704bdd403
q4a <q4arus@ya.ru>
2020-02-04 23:27:00 +0300

    linux: add dlopen and dlsym to maint. Should I add dlclose?

--- code/delta/host/main.cpp
+++ code/delta/host/main.cpp
@@ -2,10 +2,8 @@
 // Copyright (C) 2019 Force67
 
 #ifdef _WIN32
-#include <Windows.h>
-#endif
 
-#ifdef _WIN32
+#include <Windows.h>
 
 // windows platform init
 int main(int argc, char **argv) {
@@ -16,6 +14,24 @@ int main(int argc, char **argv) {
   }
 
   auto corMain = (int (*)(int, char **))GetProcAddress(hlib, "dcoreMain");
+  if (corMain)
+    return corMain(argc, argv);
+
+  return -1;
+}
+#else
+
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main(int argc, char **argv) {
+  auto *hlib = dlopen("dcore.so", RTLD_LAZY);
+  if (!hlib) {
+    fprintf(stderr, "Unable to load core: %s\n", dlerror());
+    return -1;
+  }
+
+  auto corMain = (int (*)(int, char **))dlsym(hlib, "dcoreMain");
   if (corMain)
     return corMain(argc, argv);
 
