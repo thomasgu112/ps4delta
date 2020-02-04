e525b6f8804fc46be08f65b1e43188fd7888f85c
q4a <q4arus@ya.ru>
2020-02-04 05:32:43 +0300

    style: replacing tabs with spaces

--- code/delta/host/premake5.lua
+++ code/delta/host/premake5.lua
@@ -2,36 +2,36 @@
 project "host"
     language "C++"
     kind "WindowedApp"
-	flags "NoManifest"
-	targetname "%{wks.name}"
+    flags "NoManifest"
+    targetname "%{wks.name}"
 
-	linkoptions "/ENTRY:mainCRTStartup"
+    linkoptions "/ENTRY:mainCRTStartup"
 
-	-- win/project resource description
-	defines { "rsc_company=\"Dev-Force/Alpin-Dev\"",
-			  "rsc_copyright=\"© Force67 2019-2020. All rights reserved\"",
-			  "rsc_fileversion=\"1.0.0.0\"", 
-			  "rsc_productversion=\"1.0.0.0\"",
-			  "rsc_internalname=\"%{prj.name}\"",
-			  "rsc_productname=\"PS4Delta\"",
-			  "rsc_originalname=\"%{prj.name}%{prj.targetextension}\"",
-		      "rsc_description=\"Delta PS4 emulator\"" }
+    -- win/project resource description
+    defines { "rsc_company=\"Dev-Force/Alpin-Dev\"",
+              "rsc_copyright=\"© Force67 2019-2020. All rights reserved\"",
+              "rsc_fileversion=\"1.0.0.0\"", 
+              "rsc_productversion=\"1.0.0.0\"",
+              "rsc_internalname=\"%{prj.name}\"",
+              "rsc_productname=\"PS4Delta\"",
+              "rsc_originalname=\"%{prj.name}%{prj.targetextension}\"",
+              "rsc_description=\"Delta PS4 emulator\"" }
 
-	links
-	{
-		"shared"
-	}
+    links
+    {
+        "shared"
+    }
 
     includedirs
     {
         ".",
-		"../../shared"
+        "../../shared"
     }
 
     files
     {
         "premake5.lua",
-		"_res/host.rc",
+        "_res/host.rc",
         "**.h",
         "**.cpp"
     }
