65b0fd44cf9233d97d0df7c7e31badd01b4795c6
q4a <q4arus@ya.ru>
2020-02-06 06:25:50 +0300

    linux: fix building common\utl\mem_linux.cpp

--- code/common/utl/mem_linux.cpp
+++ code/common/utl/mem_linux.cpp
@@ -11,31 +11,31 @@
 namespace utl {
     std::map<void*, size_t> memRegionSize;
 
-	void* allocMem(void* preferredAddr, size_t length, pageProtection prot, allocationType type) {
-        if(type == allocationType::commit) {
+    void* allocMem(void* preferredAddr, size_t length, page_protection prot, allocType type) {
+        if(type == allocType::commit) {
             mprotect(preferredAddr, length, (uint32_t) prot);
             return preferredAddr;
         }
-        else if(type == allocationType::reserve)
-            prot = pageProtection::priv;
+        else if(type == allocType::reserve)
+            prot = page_protection::page_private;
         void* mem;
-		mem = mmap(preferredAddr, length, (uint32_t) prot, MAP_SHARED | MAP_ANON, -1, 0);
+        mem = mmap(preferredAddr, length, (uint32_t) prot, MAP_SHARED | MAP_ANON, -1, 0);
         memRegionSize.insert(std::pair<void*, size_t> (mem, length));
         return mem;
-	}
+    }
 
-	void freeMem(void* Addr) {
-		munmap(Addr, memRegionSize[Addr]);
+    void freeMem(void* Addr) {
+        munmap(Addr, memRegionSize[Addr]);
         memRegionSize.erase(Addr);
-	}
+    }
 
-	bool protectMem(void* addr, size_t len, pageProtection prot) {
+    bool protectMem(void* addr, size_t len, page_protection prot) {
         return mprotect(addr, len, (uint32_t) prot) == 0;
-	}
+    }
 
-	size_t getAvailableMem()
-	{
-		return sysconf(_SC_PAGESIZE)*sysconf(_SC_PHYS_PAGES);
-	}
+    size_t getAvailableMem()
+    {
+        return sysconf(_SC_PAGESIZE)*sysconf(_SC_PHYS_PAGES);
+    }
 }
 #endif
